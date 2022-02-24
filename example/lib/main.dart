3ca9514e4f458ea832cb4bcdcfbc4871a4aa3953
Hao <286089659@qq.com>
2022-02-24 08:56:19 +0800

    fix: update  example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,10 +1,12 @@
+import 'package:ffmpeg_kit_flutter_full/ffmpeg_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_wcs/flutter_wcs.dart';
 import 'package:flutter_wcs/utils/enum_util.dart';
 import 'package:flutter_wcs_example/flare_kamera_page.dart';
-import 'package:flutter_wcs_example/flare_kamera_filter_widget.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:logger/logger.dart';
+// import 'package:ffmpeg_kit_flutter/ffmpeg_kit.dart';
+import 'package:path_provider/path_provider.dart';
 
 void main() {
   runApp(const DemoApp());
@@ -59,12 +61,39 @@ class _MyAppState extends State<MyApp> {
         ),
         body: Center(
           child: GestureDetector(
-            onTap: tapCamera,
+            onTap: videoToMp3,
             child: const Text('点我拍摄', style: TextStyle(fontSize: 24, color: Colors.red)),
           ),
         ),
       ),
     );
+  }
+
+  void videoToMp3() async {
+    // ffmpeg -i test.mp4 -f mp3 -vn test.mp3
+
+    final ImagePicker _picker = ImagePicker();
+    final XFile? image = await _picker.pickVideo(source: ImageSource.gallery);
+    final outPutFile = await getTemporaryDirectory();
+
+    // final formatStr = "-i ${image!.path} -vn -acodec copy ${outPutFile.path}/test12332131231432.aac";
+    // final formatStr = "-i ${image!.path} -r 5 -f image2 ${outPutFile.path}/img_%3d.jpg";
+
+    // 多段的需要分开
+    final formatStr = "-ss 00:00:00 -t 30 -i ${image!.path} ${outPutFile.path}/output_d.mp4";
+
+    print(formatStr);
+    FFmpegKit.execute(formatStr).then((value) async {
+      final returnCode = await value.getReturnCode();
+      if (returnCode?.isValueSuccess() == true) {
+        print(true);
+      } else {
+        final logs = await value.getAllLogs();
+        for (var item in logs) {
+          print("${item.getMessage()}");
+        }
+      }
+    });
   }
 
   // 拍摄
